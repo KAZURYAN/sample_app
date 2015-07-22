ae3e4c1ac102eaf9fe63a755ecb155d0e7893f53
kazuryan <kazunori.m.1986@gmail.com>
2015-07-22 13:39:49 +0000

    Finish user follow / unfollow actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,14 @@ SampleApp::Application.routes.draw do
   devise_for :users, :controllers =>{
     :registrations => "registrations"
   }
-  resources :users, only: [:destroy, :show, :index]
+  resources :users, only: [:destroy, :show, :index] do
+    member do
+      get :following, :followers
+    end
+  end
+  
   resources :microposts, only: [:create , :destroy]
+  resources :relationships, only: [:create , :destroy]
   root 'static_pages#home'
   match '/help',    to:'static_pages#help', via: 'get'
   match '/about' ,  to:'static_pages#about', via: 'get'
